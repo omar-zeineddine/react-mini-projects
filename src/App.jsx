1baa792ed15ed0c908345ab0dd93b7d231f2672b
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-26 18:22:59 +0300

    feat - fetch tours data

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,41 @@ import Loading from "./Loading";
 import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
+
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [tours, getTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
