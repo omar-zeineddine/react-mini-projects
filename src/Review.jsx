f55b174d0227be8e356f0459558950641436a9ab
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-27 20:35:15 +0300

    feat - add random click event with no repetitions

--- src/Review.jsx
+++ src/Review.jsx
@@ -33,6 +33,14 @@ const Review = () => {
     });
   };
 
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -52,7 +60,9 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">surprise me</button>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
     </article>
   );
 };
