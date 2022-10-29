6ce3ee66a4f3ecf07fed0a60ca6d366995510b76
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-29 21:25:33 +0300

    feat - set states and return app components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,21 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+        </div>
+        <div className="underline"></div>
+        <Categories />
+        <Menu />
+      </section>
+    </main>
+  );
 }
 
 export default App;
