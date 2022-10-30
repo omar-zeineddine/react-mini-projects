4b05560fe470b3d7acce89c23623a19e1ca2e42f
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-30 21:39:26 +0200

    feat - fetch jobs and set states

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
+
 function App() {
-  return <h2>tabs project setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    // console.log(newJobs);
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  return <h1>jobs</h1>;
 }
 
 export default App;
