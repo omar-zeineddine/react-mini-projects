2d27e1e8ab5a66fbde28965ddcad30cc17326d1f
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-11-09 22:53:09 +0200

    feat - add pagination buttons

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -19,12 +19,12 @@ const Characters = () => {
   // refactor - use react query
   // useQuery requires a unique query key for caching purposes
 
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(2);
 
   const fetchCharacters = async ({ queryKey }) => {
     const response = await fetch(
       // dynamically add page
-      `https://rickandmortyapi.com/api/character?=${queryKey[1]}`
+      `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
     );
     return response.json();
   };
@@ -48,6 +48,17 @@ const Characters = () => {
       {data.results.map((character) => {
         return <Character key={character.id} character={character} />;
       })}
+      <div>
+        <button
+          disabled={page === 1}
+          onClick={() => {
+            setPage((old) => old - 1);
+          }}
+        >
+          Previous
+        </button>
+        <button>Next</button>
+      </div>
     </div>
   );
 };
