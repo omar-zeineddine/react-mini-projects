954958dfede5dd600d1c224de5059cfe639e23bf
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-12-17 12:25:49 +0200

    feat - add initial project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import SingleColor from "./SingleColor";
+import { useState } from "react";
 import Values from "values.js";
+import SingleColor from "./SingleColor";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [err, setErr] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("log");
+  };
+
   return (
-    <div>
-      <h2>Color Generator</h2>
-    </div>
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={() => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <div className="container">
+          <h4>here be list</h4>
+        </div>
+      </section>
+    </>
   );
 }
 
