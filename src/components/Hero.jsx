624380adf90417aac1643d4cd35ad306aec6699a
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-01-06 17:10:23 +0200

    feat - add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,26 @@ import phone from "../assets/images/phone.svg";
 import { useGlobalContext } from "../context/context";
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>Hero</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Millions of companies of all sizes-from startups to Fortune 500s-use
+            Stripe's software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+          <button className="btn">Start Now</button>
+        </article>
+
+        <article className="hero-images">
+          <img src={phone} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
