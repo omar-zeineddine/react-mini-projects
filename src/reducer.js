116869a2f57ea7096b76cc47b0e42b8b919600c2
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-01-13 23:36:27 +0200

    feat - get item totals

--- src/reducer.js
+++ src/reducer.js
@@ -41,6 +41,26 @@ const reducer = (state, action) => {
     };
   }
 
+  if (action.type === "GET_TOTALS") {
+    // reduce that returns an object
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+
+    total = parseFloat(total.toFixed(2));
+    return { ...state, total, amount };
+  }
+
   // state: current state before update
   return state;
 };
