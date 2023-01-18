b6e324acc0745b4595bb0e50e369f02e2c9f0b05
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-01-18 18:29:51 +0200

    feat - add project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+// import components
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <h2>App</h2>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h2>nav</h2>
+    </div>
+  );
+};
+
+export default Navbar;
