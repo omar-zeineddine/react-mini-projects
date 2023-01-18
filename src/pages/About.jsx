fc7e0237d25dd2128d9c6a2ac9a1122ba044a40c
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-01-18 18:41:53 +0200

    feat - add about and error pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse asperiores
+        ratione reprehenderit id possimus ducimus molestiae voluptatibus quam
+        reiciendis maxime repellendus, provident, accusamus a explicabo totam
+        cumque aspernatur nemo repudiandae excepturi? Libero tempore tenetur
+        nisi placeat id quam iusto sapiente.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h2>error</h2>
-    </div>
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>oops!, looks like you reached a dead end</h1>
+        <Link to="/" className="btn btn-primary">
+          back to home page
+        </Link>
+      </div>
+    </section>
   );
 };
 
