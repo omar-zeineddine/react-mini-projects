b73e78eceb438d941fe6b61bc9856b031b94cd3b
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-01-18 18:38:27 +0200

    feat - add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,21 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>nav</h2>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="cocktail db" className="logo" />
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
