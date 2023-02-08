3fda8318bc6f210454a7c088f17d6526d94628bc
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-02-08 19:04:05 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
+
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <h1>Pagination</h1>
     </div>
   );
 }
