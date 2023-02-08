2743ba33e925760dd24d1fc26fdc9a3fe1efc28b
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-02-08 19:11:20 +0200

    feat - fetch and display users

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import { useFetch } from "./useFetch";
 import Follower from "./Follower";
 
 function App() {
+  const { loading, data } = useFetch();
+  console.log(data);
+
   return (
-    <div>
-      <h1>Pagination</h1>
-    </div>
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {data.map((follower) => (
+            <Follower key={follower.id} {...follower} />
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
 
