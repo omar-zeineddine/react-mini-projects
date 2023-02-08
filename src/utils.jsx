773a341a63e8d38555d71e5e9e0bb4d78319126c
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-02-08 19:21:00 +0200

    feat - add pagination logic

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,3 +1,18 @@
-const paginate = () => {};
+const paginate = (followers) => {
+  console.log("followers", followers);
+  const itemsPerPage = 9;
+  // round up and add remaining items to last page
+  const page = Math.cbrt(followers.length / itemsPerPage);
+
+  // new array of arrays with 9 items each
+  const newFollowers = Array.from({ length: page }, (_, index) => {
+    const start = index * itemsPerPage;
+    // pull out items from original array and add to new array
+    // slice(start, start + itemsPerPage) = slice(0, 9), slice(9, 18), slice(18, 27)
+    return followers.slice(start, start + itemsPerPage);
+  });
+
+  return newFollowers;
+};
 
 export default paginate;
